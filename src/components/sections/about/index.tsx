c8640eb71be006680a7ab90ea6fe3b80295c8334
HungNV <hung@m.ibis.studio>
2026-04-29 21:42:35 +0700

    complete!!!!!!!

--- src/components/sections/about/index.tsx
+++ src/components/sections/about/index.tsx
@@ -9,36 +9,60 @@ import { FaGraduationCap } from "react-icons/fa6";
 import Divider from "../divider";
 import { APP_DATA } from "@helpers/data.ts";
 import SocialMedia from "../social.media";
+import { useTranslation } from 'react-i18next';
 
 const About = () => {
+    const { t, i18n } = useTranslation();
     return (
         <>
             <Row>
                 <Col md={6} xs={12}>
-                    <h3 className="text-center mb-md-5 mb-2">Know Who <span className="brand-red">I'M</span> </h3>
-                    <div>
-                        <div>
-                            <p> Hi Everyone, I am HoiDanIT from Hanoi, VietNam.</p>
+                    {i18n.resolvedLanguage === "en" ?
+                        <>
+                            <h3 className="text-center mb-md-5 mb-2">Know Who <span className="brand-red">I'M</span> </h3>
+                            <div>
+                                <div>
+                                    <p> Hi everyone, I'm Hưng NV, a Frontend Developer based in Hanoi, Vietnam.</p>
 
-                            <p> I am currently employed as a a freelance dev (start-up).</p>
+                                    <p>I have over 5 years of experience building pixel-perfect, responsive web interfaces, with a strong focus on HTML, CSS, and JavaScript. Currently, I’m expanding my expertise in ReactJS and TypeScript to develop modern, scalable web applications.</p>
 
-                            <p> I have completed Bachelor's degree in Software Engineer at Hanoi University of Science and Technology (HUST).</p>
-                        </div>
-                    </div>
-                    <div>
-                        <p>Apart from coding, some other activities that I love to do!
+                                    <p>I graduated with a degree in Software Engineering from Hanoi University of Mining and Geology. (HUMG)</p>
+                                </div>
+                            </div>
+                            <div>
+                                <p>Beyond coding, I enjoy:
+                                </p>
+                                <ul>
+                                    <li>Playing games 🎮</li>
+                                    <li>Exploring new technologies & writing tech notes</li>
+                                    <li>Travelling</li>
+                                </ul>
+                            </div>
+                        </>
+                        :
 
-                        </p>
-                        <ul>
-                            <li>Playing Games</li>
-                            <li>Writing Tech Blogs</li>
-                            <li>Travelling</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p className="text-center brand-red">"Pursuing Your Dreams Is How You Become Homeless ~.~"</p>
-                        <p className="text-center brand-red">--Jimmy O Yang</p>
-                    </div>
+                        <>
+                            <h3 className="text-center mb-md-5 mb-2">Giới thiệu về tôi</h3>
+                            <div>
+                                <div>
+                                    <p>Chào mọi người, tôi là Hưng NV, một lập trình viên Frontend hiện đang sống tại Hà Nội, Việt Nam.</p>
+
+                                    <p>Tôi có hơn 5 năm kinh nghiệm xây dựng giao diện web responsive, hoàn hảo đến từng pixel, với trọng tâm chính là HTML, CSS và JavaScript. Hiện tại, tôi đang mở rộng kiến ​​thức chuyên môn về ReactJS và TypeScript để phát triển các ứng dụng web hiện đại, có khả năng mở rộng.</p>
+
+                                    <p>Tôi tốt nghiệp ngành Kỹ thuật Phần mềm tại Đại học Mỏ và Địa chất Hà Nội (HUMG).</p>
+                                </div>
+                            </div>
+                            <div>
+                                <p>Ngoài lập trình, tôi thích:
+                                </p>
+                                <ul>
+                                    <li>Chơi game 🎮</li>
+                                    <li>Khám phá các công nghệ mới và viết bài về công nghệ</li>
+                                    <li>Du lịch</li>
+                                </ul>
+                            </div>
+                        </>
+                    }
                 </Col >
                 <Col md={6} xs={12} className="d-flex align-items-center justify-content-center">
                     <AnimationLottie
@@ -60,21 +84,25 @@ const About = () => {
                 </Col>
                 <Col md={6} xs={12} className="mt-md-5 mt-3">
                     <div className="d-flex flex-column align-items-center gap-3">
-                        <div><h4 className="text-center brand-red">Education</h4></div>
+                        <div>
+                            <h4 className="text-center brand-red">
+                                {i18n.resolvedLanguage === "en" ? <>Education</> : <>Trình độ học vấn</>}
+                            </h4>
+                        </div>
                         <div >
                             <GlowCard identifier={`experience-5`}>
                                 <div className="p-3 relative">
                                     <div className="experience-container">
                                         <div className="duration-text">
-                                            <p>20xx-20xx</p>
+                                            <p>2013-2018</p>
                                         </div>
                                         <div className="details">
                                             <div className="icon">
                                                 <FaGraduationCap size={36} />
                                             </div>
                                             <div className="info">
-                                                <p className="title">Softwaren Engineer</p>
-                                                <p className="company">Hanoi University of Science and Technology (HUST)</p>
+                                                <p className="title">{i18n.resolvedLanguage === "en" ? <>Software Engineer</> : <>Kỹ sư phần mềm</>}</p>
+                                                <p className="company">{i18n.resolvedLanguage === "en" ? <>Hanoi University of Mining and Geology. (HUMG)</> : <>Đại học Mỏ và Địa chất Hà Nội (HUMG)</>}</p>
                                             </div>
                                         </div>
                                     </div>
--- src/components/sections/project/index.tsx
+++ src/components/sections/project/index.tsx
@@ -1,14 +1,34 @@
 import { Col, Row } from "react-bootstrap"
 import ProjectCard from "./project.card.tsx";
 import { PROJECTS } from "@helpers/data.ts";
+import { useTranslation } from 'react-i18next';
 
 const Project = () => {
+
+    const { t, i18n } = useTranslation();
     return (
         <>
             <Row>
                 <Col xs={12}>
-                    <h3 className="text-center">My Recent <span className="brand-red">Works</span> </h3>
-                    <h6 className="text-center mb-md-5 mb-2">Here are a few projects I've worked on recently.</h6>
+                    <h3 className="text-center">
+                        {i18n.resolvedLanguage === "en" ? <>
+                            My Recent <span className="brand-red">Works</span>
+                        </>
+                            :
+                            <>
+                                <span className="brand-red">Dự án</span> gần đây</>
+                        }
+                    </h3>
+                    <h6 className="text-center mb-md-5 mb-2">
+                        {
+                            i18n.resolvedLanguage === "en" ? <>
+                                Here are a few projects I've worked on recently.
+                            </> :
+                                <>
+                                    Dưới đây là một số dự án tôi đã làm gần đây.
+                                </>
+                        }
+                    </h6>
                 </Col>
             </Row>
             <Row
